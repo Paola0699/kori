3e6fa7292da127fe3c5cdecafbf841f4282cfd0d
Paola0699 <paola.pv1999@gmail.com>
2021-06-02 21:44:36 -0500

    Mail

--- mailer.php
+++ mailer.php
@@ -2,8 +2,8 @@
     $name = $_POST["name"];
     $email = $_POST["email"]
     $phone = $_POST['phone_number']
-    $mesage = $_POST['message']
+    $message = $_POST['message']
     $subject = $_POST["subject"]
 
-    echo $name . $email .  $phone . $mesage.$subject
+    mail("paola.pv1999@gmail.com", $subject, $message, "Esto es un mensaje")
 ?>
