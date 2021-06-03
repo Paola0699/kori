adf9d8f7ffa357e6aa99d15b68cdb870677d7818
Paola0699 <paola.pv1999@gmail.com>
2021-06-02 21:21:45 -0500

    Hola

--- mailer.php
+++ mailer.php
@@ -1,5 +1,7 @@
 <?php
     // Only process POST reqeusts.
+    echo $_POST["name"]
+    echo $POST["email"]
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
